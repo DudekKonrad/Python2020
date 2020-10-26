28ef7dd941690f23c3aced93ba221caede718981
kolomek <63727689+kolomek@users.noreply.github.com>
2020-10-26 19:37:27 +0100

    Update 2.17.py

--- 2.17.py
+++ 2.17.py
@@ -3,5 +3,9 @@ line = "Guido van Rossum is a Dutch programmer\n" \
        "best known as the creator of the Python programming language.\n" \
        "He is currently a member of the Python Steering Council.\n"
 result = re.findall(r'\w+', line)
-sorted_list = sorted(result, key=str.lower)
-print(sorted_list)
+sorted_alphabetically = sorted(result, key=str.lower)
+sorted_by_length = sorted(result, key=len)
+print("Sorted alphabetically: ")
+print(sorted_alphabetically)
+print("Sorted by length:")
+print(sorted_by_length)
