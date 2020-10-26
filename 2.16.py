6a459d46cfe34e4690f2c2c2871ba5f336cedd78
kolomek <63727689+kolomek@users.noreply.github.com>
2020-10-26 19:32:45 +0100

    Update 2.16.py

--- 2.16.py
+++ 2.16.py
@@ -1,7 +1,6 @@
 line = "GvR is a Dutch programmer\n" \
        "best known as the creator of the Python programming language.\n" \
        "He is currently a member of the Python Steering Council.\n"
-print(line)
 print("Before replace:")
 print(line)
 result = line.replace("GvR", "Guido van Rossum")
